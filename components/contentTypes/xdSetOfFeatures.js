96de9335037a3b25df81c03d6a5ee6dc5e63ea84
Asier del Pozo <adelpozo@xtivia.com>
2022-01-18 14:50:24 -0500

    Classnames package added

--- components/contentTypes/xdSetOfFeatures.js
+++ components/contentTypes/xdSetOfFeatures.js
@@ -13,7 +13,7 @@ export default function xdSetOfFeatures({entry}){
         case "Display Style 3":
             return ( <FeatureC entry={entry} /> );
         default :
-           // console.log("Display style not found: " + displayStyle);
+            //console.log("Display style not found: " + displayStyle);
             //console.log("Loading default display style");
             return ( <FeatureA entry={entry} /> );
     }
